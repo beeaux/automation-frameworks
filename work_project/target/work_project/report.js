75dbd54ffd4f3dd03ec97076f18995a2ed8238c7
Ade Bakre <adebakre@gmail.com>
2013-05-13 00:30:50 +0100

    Changes to data
    Made some changes

---
+++ work_project/target/work_project/report.js
@@ -0,0 +1,161 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri('automation\tests\EmailLogInSample.feature');
+formatter.feature({
+  "id": "simple-log-to-email-site-page",
+  "tags": [
+    {
+      "name": "@SampleTests",
+      "line": 1
+    }
+  ],
+  "description": "",
+  "name": "Simple log to email site page",
+  "keyword": "Feature",
+  "line": 3
+});
+formatter.scenario({
+  "id": "simple-log-to-email-site-page;user-is-able-to-log-into-the-outlook-site;;2",
+  "tags": [
+    {
+      "name": "@SampleTests",
+      "line": 1
+    }
+  ],
+  "description": "",
+  "name": "User is able to log into the outlook site",
+  "keyword": "Scenario Outline",
+  "line": 14,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "I am on the Hotmail homepage",
+  "keyword": "Given ",
+  "line": 6,
+  "matchedColumns": [
+    0
+  ]
+});
+formatter.step({
+  "name": "I have a valid email account",
+  "keyword": "And ",
+  "line": 7,
+  "matchedColumns": [
+    1
+  ]
+});
+formatter.step({
+  "name": "I enter my Hotmail adebakre@hotmail.com and password",
+  "keyword": "When ",
+  "line": 8,
+  "matchedColumns": [
+    0,
+    2,
+    3
+  ]
+});
+formatter.step({
+  "name": "I click on the sign in button",
+  "keyword": "And ",
+  "line": 9
+});
+formatter.step({
+  "name": "I should be logged in",
+  "keyword": "Then ",
+  "line": 10,
+  "matchedColumns": [
+    4
+  ]
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "id": "simple-log-to-email-site-page;user-is-able-to-log-into-the-outlook-site;;3",
+  "tags": [
+    {
+      "name": "@SampleTests",
+      "line": 1
+    }
+  ],
+  "description": "",
+  "name": "User is able to log into the outlook site",
+  "keyword": "Scenario Outline",
+  "line": 15,
+  "type": "scenario"
+});
+formatter.step({
+  "name": "I am on the Gmail homepage",
+  "keyword": "Given ",
+  "line": 6,
+  "matchedColumns": [
+    0
+  ]
+});
+formatter.step({
+  "name": "I have a valid email account",
+  "keyword": "And ",
+  "line": 7,
+  "matchedColumns": [
+    1
+  ]
+});
+formatter.step({
+  "name": "I enter my Gmail adebakre@gmail.com and password",
+  "keyword": "When ",
+  "line": 8,
+  "matchedColumns": [
+    0,
+    2,
+    3
+  ]
+});
+formatter.step({
+  "name": "I click on the sign in button",
+  "keyword": "And ",
+  "line": 9
+});
+formatter.step({
+  "name": "I should be logged in",
+  "keyword": "Then ",
+  "line": 10,
+  "matchedColumns": [
+    4
+  ]
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
