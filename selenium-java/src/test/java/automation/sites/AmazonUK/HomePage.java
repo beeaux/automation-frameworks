d2ef9a8a03f361183409b1b1bb86728265f332c1
BEE AUX <yomi.folami@beeaux.com>
2013-06-27 17:03:54 +0100

    AMZN-01  ------------ visa interview tests

--- selenium-java/src/test/java/automation/sites/AmazonUK/HomePage.java
+++ selenium-java/src/test/java/automation/sites/AmazonUK/HomePage.java
@@ -1,17 +1,23 @@
-package
+package automation.sites.AmazonUK;
 
+
+import automation.tests.SharedDriver;
+import org.openqa.selenium.WebDriver;
+
+import static automation.core.WebDriverExtensions.*;
 
 public class HomePage {
   private final SharedDriver Driver;
   private static String url = System.getProperty("url");
   
-  public Homepage(WebDriver driver) {
+  public HomePage(WebDriver driver) {
     /*
       note: url is configurable through system property configuration, as shown above.
     */
     navigateTo(url);
-    waitUntilPageTitle("Amazon.co.uk");
+    //waitUntilPageTitle("Amazon.co.uk");
     this.Driver = (SharedDriver)driver;
   }
+}
   
   
