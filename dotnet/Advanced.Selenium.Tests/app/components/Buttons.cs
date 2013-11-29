dc1ab9dd03fc74012187a6e96a4445fa7d5d4f7b
BEE AUX <yomi.folami@beeaux.com>
2013-11-29 17:06:17 +0000

    Create Buttons.cs

---
+++ dotnet/Advanced.Selenium.Tests/app/components/Buttons.cs
@@ -0,0 +1,12 @@
+using Arena.EnhancedNews.Regression.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace Arena.EnhancedNews.Regression.Pages
+{
+    [Binding]
+    public class Buttons
+    {
+    }
+}
